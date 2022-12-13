39ae055682a91f1f2419b772d9642dcea11469fc
DoodMac <48062579+jbird84@users.noreply.github.com>
2022-12-13 10:56:34 -0600

    Show how to use a UIColorWell

--- ColorWellTemplate/ViewController.swift
+++ ColorWellTemplate/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var viewToChangeColor: UIView!
+    @IBOutlet weak var colorWell: UIColorWell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewToChangeColor.layer.cornerRadius = viewToChangeColor.frame.size.height / 2
+        setupColorWell()
+    }
+    
+    private func setupColorWell() {
+        colorWell.title = "Pick Your Color"
+        colorWell.addTarget(self, action: #selector(colorChanged), for: .valueChanged)
     }
 
+    @objc private func colorChanged() {
+        viewToChangeColor.backgroundColor = colorWell.selectedColor
+        
+        //if you want to get hex value grab it like this
+        guard let hexvalue = colorWell.selectedColor?.toHex else { return }
+        print(hexvalue)
+    }
 
 }
 
