5a9ce46c09b0790b9f044e18f7c03d3f48113118
DoodMac <48062579+jbird84@users.noreply.github.com>
2022-12-13 10:40:52 -0600

    Initial Commit

---
+++ ColorWellTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorWellTemplate
+//
+//  Created by Kinney Kare on 12/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
